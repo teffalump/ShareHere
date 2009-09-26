d6c134cfa77fe3b373bd78fbebd1bd0b426f3130
_shade <timelessmomentum@gmail.com>
2009-09-26 09:32:47 -0600

    Parameterized statements and some other minor changes

--- users.php
+++ users.php
@@ -8,16 +8,20 @@
 if (isset($_POST['network_id'])) //network_id or w/e identifying mark
 {
     require_once "general.php";
-    $network_id=$_POST['network_id'];
-    $query=sprintf("SELECT name FROM users WHERE network_id='%s'", msyql_real_escape_string($network_id)); //add more fields if one wants and also modify to include child networks, but we need to decide what schema to use first
-    $result=mysql_query($query);
-    while( $row = mysql_fetch_array($result) )
-    {
-        $users[] = $row[0]; //Is this the best way...? I have no idea. =-/
-    }
-    echo json_encode($users);
-    mysql_free_result($result);
-    exit;
+    require_once "connection.php";
+    $stmt=mysqli_stmt_init($link)
+    if (mysqli_stmt_prepare($stmt, "SELECT name FROM users WHERE network_id=?")) {
+        mysqli_stmt_bind_param($stmt,"i",$network_id);    
+        $network_id=$_POST['network_id'];
+        mysqli_stmt_execute($stmt); //add more fields if one wants and also modify to include child networks, but we need to decide what schema to use first
+        mysqli_stmt_bind_result($stmt, $name);
+        while(mysqli_stmt_fetch($stmt))
+            {
+            $users[] = $name; //Is this the best way...? I have no idea. =-/
+            }
+        echo json_encode($users);
+        mysqli_stmt_close($stmt);
+        exit;
 } 
 else 
 {
