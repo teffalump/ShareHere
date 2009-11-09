00d49e05fbdfe3f552fc2b6aa50c3f33deb95413
_shade <timelessmomentum@gmail.com>
2009-11-08 21:14:39 -0700

    some corrections

--- connection.php
+++ connection.php
@@ -1,4 +1,9 @@
 <?php
+define ("SERVER", "localhost");
+define ("S_USERNAME", "root");
+define ("S_PASSWORD", "yummy");
+define ("DATABASE", "goodies");
+
 //Connect to server and open database object
 $link = @mysqli_connect(SERVER, S_USERNAME, S_PASSWORD, DATABASE);
 /* check connection */
